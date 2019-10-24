8e4655f98e7b919ca14d61ad43ec3e56dc686f61
antonina18 <antonina18@o2.pl>
2019-10-24 15:12:43 +0200

    fix slave8

--- slave-java-8/startup.sh
+++ slave-java-8/startup.sh
@@ -3,4 +3,4 @@
 bash ./wait-for-it.sh ${JENKINS_MASTER_SERVER}:${JENKINS_MASTER_PORT} -t 300
 bash ./wait-for-it.sh ${JENKINS_MASTER_SERVER}:${JENKINS_MASTER_JNLP_PORT} -t 300
  
-java -jar nbiot-gateway-efento.jar -jnlpUrl http://${JENKINS_MASTER_SERVER}:${JENKINS_MASTER_PORT}/computer/${SLAVE_NAME}/slave-agent.jnlp -secret ${JENKINS_TOKEN} -workDir /opt
+java -jar slave8.jar -jnlpUrl http://${JENKINS_MASTER_SERVER}:${JENKINS_MASTER_PORT}/computer/${SLAVE_NAME}/slave-agent.jnlp -secret ${JENKINS_TOKEN} -workDir /opt

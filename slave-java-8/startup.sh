f896fbbf407553eea9e47b3e3c1309505232a212
antonina18 <antonina18@o2.pl>
2019-10-25 09:00:02 +0200

    cleaning up

--- slave-java-8/startup.sh
+++ slave-java-8/startup.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/sh
- 
-bash ./wait-for-it.sh ${JENKINS_MASTER_SERVER}:${JENKINS_MASTER_PORT} -t 300
-bash ./wait-for-it.sh ${JENKINS_MASTER_SERVER}:${JENKINS_MASTER_JNLP_PORT} -t 300
- 
+  
 java -jar slave8.jar -jnlpUrl http://${JENKINS_MASTER_SERVER}:${JENKINS_MASTER_PORT}/computer/${SLAVE_NAME}/slave-agent.jnlp -secret ${JENKINS_TOKEN} -workDir /opt
